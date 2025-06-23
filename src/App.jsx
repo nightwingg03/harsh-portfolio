67c58c3ad32804a35a4c40a4b01a700621faa05c
Harsh Srivastava <harshsrivastava10b@gmail.com>
2025-06-23 12:40:32 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Navbar from "./sections/Navbar";
+import Hero from "./sections/Hero";
+import About from "./sections/About"
+import Contact from "./sections/Contact"
+import Footer from "./sections/Footer";
+const App = () =>{
+  return(
+    <div className="container mx-auto max-w-7xl">
+      <Navbar />
+      <Hero />
+      <About/>
+      <Contact/>
+      <Footer/>
+    </div>
+  )
+}
+export default App;
---
+++ src/constants/index.js
@@ -0,0 +1,17 @@
+export const mySocials = [
+  {
+    name: "WhatsApp",
+    href: "",
+    icon: "/assets/socials/whatsApp.svg",
+  },
+  {
+    name: "Linkedin",
+    href: "https://www.linkedin.com/in/harsh-srivastava03//",
+    icon: "/assets/socials/linkedIn.svg",
+  },
+  {
+    name: "Instagram",
+    href: "https://www.instagram.com/fulpower02/reels/",
+    icon: "/assets/socials/instagram.svg",
+  },
+];
