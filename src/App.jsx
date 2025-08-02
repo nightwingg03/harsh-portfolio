ce9310089e2a9015f4683d7d2f3fc8b7ee2c1cff
Harsh Srivastava <harshsrivastava10b@gmail.com>
2025-08-02 19:03:04 +0530

    Revert "Merge branch 'main' of https://github.com/nightwingg03/harsh-portfolio"
    This reverts commit 4fba1bd5f261e7a819b7fc31f0876ab8c3781c11, reversing
    changes made to b77ef432f7b022e5b14cb345a20f34c1de88e192.

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,18 @@
 import React from "react";
-import { HashRouter } from "react-router-dom"; // ⬅️ Import HashRouter
 import Navbar from "./sections/Navbar";
 import Hero from "./sections/Hero";
-import About from "./sections/About";
-import Contact from "./sections/Contact";
+import About from "./sections/About"
+import Contact from "./sections/Contact"
 import Footer from "./sections/Footer";
-
-const App = () => {
-  return (
-    <HashRouter>
-      <div className="container mx-auto max-w-7xl">
-        <Navbar />
-        <Hero />
-        <About />
-        <Contact />
-        <Footer />
-      </div>
-    </HashRouter>
-  );
-};
-
+const App = () =>{
+  return(
+    <div className="container mx-auto max-w-7xl">
+      <Navbar />
+      <Hero />
+      <About/>
+      <Contact/>
+      <Footer/>
+    </div>
+  )
+}
 export default App;
